86d6cbb1dffc0321027b629e9385a926dc173bc6
Ligen Raj <ligenraj@Ligens-MacBook-Pro.local>
2025-07-03 19:46:08 +0530

    Initial commit

---
+++ Design Styles/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Design Styles
+//
+//  Created by Ligen Raj on 03/07/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Design Styles/Design_StylesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Design_StylesApp.swift
+//  Design Styles
+//
+//  Created by Ligen Raj on 03/07/25.
+//
+
+import SwiftUI
+
+@main
+struct Design_StylesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
